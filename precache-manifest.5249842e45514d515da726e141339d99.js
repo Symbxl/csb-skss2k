90a399170e526af776daa875c5f63a9d263f1d62
Code Sandbox <hello@codesandbox.io>
2022-10-07 20:08:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5249842e45514d515da726e141339d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edb451dc9db08a99355cda7dbef7205",
+    "url": "https://symbxl.github.io/csb-skss2k/index.html"
+  },
+  {
+    "revision": "34a3fda90884c6791733",
+    "url": "https://symbxl.github.io/csb-skss2k/static/css/main.3e5b39de.chunk.css"
+  },
+  {
+    "revision": "d22d8c50e86497dfb796",
+    "url": "https://symbxl.github.io/csb-skss2k/static/js/2.72dba023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://symbxl.github.io/csb-skss2k/static/js/2.72dba023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a3fda90884c6791733",
+    "url": "https://symbxl.github.io/csb-skss2k/static/js/main.68e371c0.chunk.js"
+  },
+  {
+    "revision": "2e22fad82e0b51207aa2",
+    "url": "https://symbxl.github.io/csb-skss2k/static/js/runtime-main.44b9f86a.js"
+  }
+]);
